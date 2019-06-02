285ce23f1365821021b6a9b4e11b0fa5d0e0b61e
y047aka <y047aka@gmail.com>
2019-06-02 15:09:07 +0900

    Initial commit

---
+++ src/elm/Main.elm
@@ -0,0 +1,77 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, div, footer, h1, img, node, p, text)
+import Html.Attributes exposing (class, src)
+
+
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { userState : String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model ""
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Elm 0.19 starter"
+    , body =
+        [ siteHeader
+        , node "main"
+            []
+            [ img [ src "/images/Elm_logo.png" ] []
+            ]
+        , siteFooter
+        ]
+    }
+
+
+siteHeader : Html Msg
+siteHeader =
+    Html.header [ class "site-header" ]
+        [ h1 [] [ text "Site Title" ]
+        ]
+
+
+siteFooter : Html Msg
+siteFooter =
+    footer [ class "site-footer" ]
+        [ p [ class "copyright" ] [ text "© 2019 y047aka" ]
+        ]
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+window.onload = () => {
+	Elm.Main.init()
+}
