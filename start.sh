15c63120f5c247ca95b9c2d4252d30261d002d94
Yoshitaka Totsuka <yoshitaka.totsuka@siiibo.com>
2020-07-23 22:47:29 +0900

    Uninstall `npm-run-all`

--- start.sh
+++ start.sh
@@ -5,4 +5,8 @@ rm -rf docs public
 npx sass src:docs
 
 # Watch
-npx npm-run-all -p watch:*
+npx cpx -w src/index.html docs &
+npx cpx -w \"src/assets/**/*\" docs/assets &
+npx cpx -w src/main.js docs &
+npx sass --watch src:docs &
+npx elm-live src/Main.elm --open --start-page=index.html --dir=docs -- --output=docs/elm.js
